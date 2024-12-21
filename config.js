425a85a1195caa599a12303ac9aa62dddc7da7bc
Darshanawee <darshanaweerawardana@gmail.com>
2024-12-21 06:36:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "TioggCTJ#mrXvLyroExhAn1uTU2C0F4alSBrRTXEj6Y0zjj4zLpk", // Add your session id
 MONGODB: process.env.MONGODB || "mongodb+srv://laramd:sadeesha2006@anya.gvsp435.mongodb.net/?retryWrites=true&w=majority&appName=Anya", // Add your mongodb url
 };
