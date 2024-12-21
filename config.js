567702a800bcfffcdde6f8dd627be7a9abecba31
Darshanawee <darshanaweerawardana@gmail.com>
2024-12-21 07:43:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 function convertToBool(text, fault = 'true') {
-    return text === fault ? true : false;
+    return text === fault =' true ';
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "TioggCTJ#mrXvLyroExhAn1uTU2C0F4alSBrRTXEj6Y0zjj4zLpk", // Add your session id
